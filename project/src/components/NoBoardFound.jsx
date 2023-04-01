3ec8719b0d4d0800b9d2cd6c27dea1f6f868f557
joi-lightyears <kunthanhdat@gmail.com>
2023-04-01 13:43:45 +0700

    fix problem when delete board

--- project/src/components/NoBoardFound.jsx
+++ project/src/components/NoBoardFound.jsx
@@ -1,11 +1,19 @@
-import React,{useState} from 'react'
+import React,{useState, useEffect} from 'react'
 import {BiPlusMedical} from 'react-icons/bi'
 import ModalAddBoard from './ModalAddBoard';
+import { updateData } from '../redux/updateData';
+import { boardsSelector } from '../redux/selectors';
+import { useSelector} from 'react-redux';
 
-function NoBoardFound() {
+
+function NoBoardFound({currentUser, setActiveCollect}) {
 
   const [showModalAddBoard, setShowModalAddBoard] = useState(false)
-  
+  const boards = useSelector(boardsSelector);
+
+  useEffect(() => { 
+    updateData(boards, currentUser);
+  }, [boards, currentUser]);
   return (
     <div className="no-board-found">
       <div className="container">
@@ -14,7 +22,7 @@ function NoBoardFound() {
           <BiPlusMedical/> Create New Board
         </div>
       </div>
-      {showModalAddBoard && <ModalAddBoard setShowModalAddBoard={setShowModalAddBoard}/>}
+      {showModalAddBoard && <ModalAddBoard currentUser={currentUser} setShowModalAddBoard={setShowModalAddBoard} setActiveCollect={setActiveCollect}/>}
     </div>
   )
 }
